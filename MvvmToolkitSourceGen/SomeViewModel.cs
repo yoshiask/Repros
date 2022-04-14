da3d9b1663dd6acfc14bc9831ac82b5a9708451e
Yoshi Askharoun <jjask7@gmail.com>
2022-04-13 23:09:49 -0500

    MVVM Toolkit source generator issues

---
+++ MvvmToolkitSourceGen/SomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+
+namespace MvvmToolkitSourceGen
+{
+    public class SomeViewModel : ObservableObject
+    {
+        private bool _isTrue;
+
+        public bool IsTrue
+        {
+            get => _isTrue;
+            set => SetProperty(ref _isTrue, value);
+        }
+    }
+}
