f3dc3aac9d920e367d7ac7a32ed7bc24cdd29c16
Yoshi Askharoun <jjask7@gmail.com>
2022-07-07 19:34:40 -0500

    Added console app test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(delegate
+            {
+                string localAppData = @"C:\Users\jjask\AppData\Local\FluentStoreBeta";
+                bool exists = Directory.Exists(localAppData);
+            });
+        }
+    }
+}
